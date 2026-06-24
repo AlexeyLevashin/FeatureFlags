40bcfad87e7fab39c9e11604048a119d171e93f5
geshaman <glebovyaroslav58@gmail.com>
2026-06-24 14:26:53 +0300

    feat: add auth handler and jwt

--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module FeatureFlags
+
+go 1.26.4
+
+require github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
---
+++ handlers/middleware.go
@@ -0,0 +1,19 @@
+package handlers
+
+import (
+	"net/http"
+	"strings"
+)
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		token := strings.TrimPrefix(authHeader, "Bearer ")
+		if token == "" {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
