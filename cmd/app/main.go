d695c2ec2d16c1b7d15dd619fa8b73e2b1c6793b
geshaman <glebovyaroslav58@gmail.com>
2026-06-24 18:26:13 +0300

    refactor: move handlers, remove .idea

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"FeatureFlags/handlers"
+	http2 "FeatureFlags/internal/transport/http"
 	"fmt"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/auth/login", handlers.LoginHandler)
+	http.HandleFunc("/auth/login", http2.LoginHandler)
 
 	fmt.Println("running on port 8080")
 	http.ListenAndServe(":8080", nil)
