5fa447eee3f71690b70882867dfad8acbcccfc04
geshaman <glebovyaroslav58@gmail.com>
2026-06-25 14:05:06 +0300

    feat: create user repo with method FindByEmail

--- internal/repository/user_repo.go
+++ internal/repository/user_repo.go
@@ -1,1 +1,11 @@
 package repository
+
+import "FeatureFlags/internal/domain"
+
+type UserRepository struct{}
+
+// func NewUserRepository() *UserRepository {}
+
+func (r *UserRepository) FindByEmail(email string) (domain.User, error) {
+	return domain.User{}, nil
+}
