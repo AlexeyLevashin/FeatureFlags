aecf44693de20b4177d8246c00d4e6a201792fe8
geshaman <glebovyaroslav58@gmail.com>
2026-06-30 03:06:07 +0300

    refactor: move auth DTO

---
+++ internal/dto/user.go
@@ -0,0 +1,16 @@
+package dto
+
+type LoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type LoginResponse struct {
+	Token string `json:"token"`
+}
+
+type GetMeResponse struct {
+	Email   string `json:"email"`
+	Name    string `json:"name"`
+	Surname string `json:"surname"`
+}
