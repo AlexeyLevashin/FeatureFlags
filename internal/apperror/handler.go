e27a4c958d2637f9d4931c71a75435748fe5b61b
geshaman <glebovyaroslav58@gmail.com>
2026-06-30 14:17:44 +0300

    fix: ignore encoder exception

--- internal/apperror/handler.go
+++ internal/apperror/handler.go
@@ -11,9 +11,9 @@ func HandleError(w http.ResponseWriter, err error) {
 	w.Header().Set("Content-Type", "application/json")
 	if errors.As(err, &appErr) {
 		w.WriteHeader(appErr.Code)
-		json.NewEncoder(w).Encode(map[string]string{"error": appErr.Message})
+		_ = json.NewEncoder(w).Encode(map[string]string{"error": appErr.Message})
 		return
 	}
 	w.WriteHeader(http.StatusInternalServerError)
-	json.NewEncoder(w).Encode(map[string]string{"error": "внутренняя ошибка сервера"})
+	_ = json.NewEncoder(w).Encode(map[string]string{"error": "внутренняя ошибка сервера"})
 }
